bd87f20a680b11c1cd6fa274853ee9b630d95ab6
VincentMcLoughlin <vincemcloughlin@hotmail.com>
2022-06-10 23:57:57 -0400

    Adding initial data read setup

---
+++ create_dataset.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+import typing
+from typing import Any, Tuple
+
+import tensorflow as tf
+
+import tensorflow_text as tf_text
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+import pathlib
+
+class Dataset():
+
+    def __init__(self):
+        self.input = []
+        self.target = []
+        self.tf_dataset = []
+
+    def load_data(self, path):
+        text = path.read_text(encoding='utf-8')
+        lines = text.splitlines()
+
+        pairs = [line.split('\t') for line in lines]
+
+        inp = [inp for targ, inp in pairs]
+        targ = [targ for targ, inp in pairs]        
+
+        return inp, targ
+
+    def create_dataset(self, download=False):
+
+        if download == True:
+            path_to_zip = tf.keras.utils.get_file('spa-eng.zip', origin='http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip', extract=True)            
+            path_to_file = pathlib.Path(path_to_zip).parent/'spa-eng/spa.txt'
+
+        path_to_file = pathlib.Path('spa-eng/spa.txt')
+
+        self.input, self.target = self.load_data(path_to_file)
+        print(self.input[1])
+        print(self.target[-1])
+
+        BUFFER_SIZE = len(self.input)
+        BATCH_SIZE = 64
+
+        dataset = tf.data.Dataset.from_tensor_slices((self.input, self.target)).shuffle(BUFFER_SIZE)
+        dataset = dataset.batch(BATCH_SIZE)
+        self.tf_dataset = dataset
+        tf.data.experimental.save(
+            self.tf_dataset, "spanish_english_dataset", compression=None, shard_func=None, checkpoint_args=None
+        )
+
+        print(f"Saved dataset")
+        return dataset, self.input, self.target
---
+++ vectorize_text.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+import typing
+from typing import Any, Tuple
+
+import tensorflow as tf
+
+import tensorflow_text as tf_text
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+import pathlib
+
+def tf_lower_and_split_punctuation(text):
+    #split accented characters
+    text = tf_text.normalize_utf8(text, 'NFKD')
+    text = tf.strings.lower(text)
+
+    #Keep space, a to z and some punctuation
+    text = tf.strings.regex_replace(text, '[^ a-z.?!,¿]', '')
+
+    #Add spaces around punctuation
+    text = tf.strings.regex_replace(text, '[.?!,¿]', r' \0 ')
+
+    text = tf.strings.strip(text)
+
+    text = tf.strings.join(['[START]', text, '[END]'], separator=' ')
+    return text
+
+def vectorize_text(input):
+
+    max_vocab_size = 5000
+    text_processor = tf.keras.layers.TextVectorization(
+    standardize=tf_lower_and_split_punctuation, max_tokens=max_vocab_size)
+
+    text_processor.adapt(input)
+
+    return text_processor
+
+## Text preprocessing
+
+#Need to standardize 
+# example_text = tf.constant('¿Todavía está en casa?')
+
+# print(example_text.numpy())
+# print(tf_text.normalize_utf8(example_text, 'NFKD').numpy())
+
+# print(example_text.numpy().decode())
+# print(tf_lower_and_split_punctuation(example_text).numpy().decode())
